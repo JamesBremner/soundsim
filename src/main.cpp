c1b43461c57b2cb1679527d3e7eb01f53bab467e
JamesBremner <james@ravenspoint.com>
2022-10-17 20:51:43 -0400

    Document class interfaces

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 class cGrid;
 
+/// @brief A location in 3D space
 class cNode
 {
 public:
@@ -16,35 +17,84 @@ public:
     double myPressure;
     double myVx, myVy, myVz;
 
+    /// @brief Update the values of the node
+    /// @param prev pointer to grid of nodes at previous time step
+
     void update(cGrid *prev);
+
+    /// @brief text display of node values
+    /// @return text
     std::string text();
+
+    /// @brief set node pressure
+    /// @param p 
     void pressure(double p)
     {
         myPressure = p;
     }
 };
+/// @brief A 3D grid of locations
 class cGrid
 {
 public:
     std::vector<cNode> myGrid;
     int myNx, myNy, myNz;
     int myTime;
+
+    /// @brief construct grid
+    /// @param Nx number of nodes in x direction
+    /// @param Ny number of nodes in y direction
+    /// @param Nz number of nodes in x direction
+    /// @param time time of simulation
     cGrid(
         int Nx, int Ny, int Nz,
         int time);
+
+    /// @brief Impose a time source
+    /// @param n Node with surce values
+
     void source(const cNode &n);
+    
+    /// @brief update node values
+    /// @param prev pointer to grid of nodes with previous time step values
     void update(cGrid *prev);
+
+    /// @brief display od node values
+    /// @return text 
+
     std::string text();
 
+    /// @brief reference to node at a location
+    /// @param x 
+    /// @param y 
+    /// @param z 
+    /// @return node reference
     cNode &node(int x, int y, int z);
 };
+
+/// @brief A Finite Difference Time Domain simulator
 class cSim
 {
 public:
+    /// @brief Construct simulation of specified size
+    /// @param Nx 
+    /// @param Ny 
+    /// @param Nz 
+
     cSim(int Nx, int Ny, int Nz);
+
+    /// @brief Initialize the simulation
     void init();
+
+    /// @brief Impose a source
     void source();
+
+    /// @brief Advance simulatiobn one time step
     void step();
+
+    /// @brief Text display of simulation state
+    /// @return text
+    
     std::string text();
 
 private:
